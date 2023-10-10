99819b2296f5ac938099a43790d927ce7faf535e
Tan <ami.o2sx@gmail.com>
2023-10-11 00:20:31 +0700

    Close DB is fail connection

--- Helpers/DatabaseHelper.js
+++ Helpers/DatabaseHelper.js
@@ -1,23 +1,27 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: process.env.STORE_DB_HOST,
-    user: process.env.STORE_DB_USER,
-    password: process.env.STORE_DB_PASSWORD,
-    database: process.env.STORE_DB_NAME,
-    connectTimeout: 1000 // 1 second
-});
-
 class DatabaseHelper {
     static async getData(sql, values, single = false) {
+        const connection = mysql.createConnection({
+            host: process.env.STORE_DB_HOST,
+            user: process.env.STORE_DB_USER,
+            password: process.env.STORE_DB_PASSWORD,
+            database: process.env.STORE_DB_NAME,
+            connectTimeout: 1000 // 1
+        });
+
         return new Promise((resolve, reject) => {
             connection.query(sql, values, (error, results) => {
-                if (error) reject(error);
+                if (error) {
+                    reject(error);
+                }
                 else {
                     if (single) resolve(results[0]);
                     else resolve(results)
                 }
             });
+        }).finally(() => {
+            connection.end();
         });
     }
 }
